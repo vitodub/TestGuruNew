835f28f08acc40fe1e9a01d6acb31ff92ddc3248
Vitaliy Dubrovskiy <dubrovskiyvitaliy@yandex.ru>
2025-06-05 17:12:46 +0500

    feat: added scopes and validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,13 @@
 class Question < ApplicationRecord
   belongs_to :test
-  has_many :answers
+
+  has_many :answers, dependent: :destroy
+  
+  validates :body, presence: true
+
+  validate :validate_answers_number
+
+  def validate_answers_number
+    errors.add(:answers_number) if answers.count.to_i != 1..4
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   has_many :usertests
-  has_many :tests, through: :usertests
+  has_many :tests, through: :usertests, dependent: :destroy
   has_many :authortests, class_name: :Test, foreign_key: :author_id
+
+  validates :email, presence: true
+
+  scope :tests_by_difficulty, -> (level) { where(level: level) }
 
   def tests_by_difficulty(level)
     tests.where(level: level)
